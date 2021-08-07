ad4e8211a545b26a9448fd8de46e20dcabd8ee3f
MrBarskih <ilyabarskih1@gmail.com>
2021-08-07 19:49:30 +0700

    Added rigid animation

--- Assets/Scripts/AI/Bot.cs
+++ Assets/Scripts/AI/Bot.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 using UnityEngine.AI;
 
 
-public class BotMovement : MonoBehaviour
+public class Bot : MonoBehaviour
 {
+    private enum state 
+    {
+        Idle,
+        Movement
+    }
     private NavMeshAgent meshAgent;
     [SerializeField]
     private Transform target;
+    private state botState = state.Movement;
     private void Awake()
     {
         meshAgent = GetComponent<NavMeshAgent>();
@@ -14,6 +20,20 @@ public class BotMovement : MonoBehaviour
 
     private void Update()
     {
-        meshAgent.SetDestination(target.position);
+        switch (botState) 
+        {
+            case state.Idle:
+                
+            break;
+
+            case state.Movement:
+                meshAgent.SetDestination(target.position);
+                if (transform.position.magnitude < 1f) 
+                {
+                    meshAgent.SetDestination(transform.position);
+                    botState = state.Idle;
+                }
+            break;
+        }
     }
 }
